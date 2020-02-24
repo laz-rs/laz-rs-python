1d1fb7061aacb418d751bbd4f7f9575657a1614f
tmontaigu <thomas.montaigu@laposte.net>
2020-02-24 16:01:38 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pylaz',
+    version='0.1.0',
+    packages=[''],
+    license='MIT',
+    author='Thomas Montaigu',
+    author_email='thomas.montaigu@laposte.net',
+    description='Python bindings to laz-rs',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+    ],
+)
