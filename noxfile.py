58b21ce148850291af82439cb7087f355df52a94
tmontaigu <thomas.montaigu@laposte.net>
2020-12-21 12:54:20 +0100

    add noxfile to build wheels

---
+++ noxfile.py
@@ -0,0 +1,24 @@
+import nox
+import os
+from pathlib import Path
+import shutil
+
+WHEEL_DIR = Path(os.environ.get("CARGO_TARGET_DIR", '.target')) / "wheels"
+
+
+@nox.session(python=['3.6', '3.7', '3.8', '3.9'])
+def build_wheel(session):
+    session.install('maturin')
+    session.run('cargo', 'clean', external=True)
+    session.run('maturin', 'build', '--interpreter', 'python', '--release')
+
+    wheels = list(WHEEL_DIR.glob('*.whl'))
+    assert len(wheels) == 1
+    wheel = str(wheels[0])
+
+    # quick test of the wheel
+    session.install(wheel)
+    session.run('python', '-c', 'import lazrs')
+
+    # Save the wheel as its going to be erased by the next cargo clean
+    shutil.copy(wheel, '.')
